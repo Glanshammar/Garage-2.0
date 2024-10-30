3c544e282cac9c40ce8569b76c2c6452d23db4d3
Karin Wegner <karinwegner00@gmail.com>
2024-10-30 10:21:09 +0100

    Added IndexViewModel

---
+++ Garage 2.0/Models/ViewModels/ParkedVehicleIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Garage_2._0.Models.ViewModels
+{
+    public class ParkedVehicleIndexViewModel
+    {
+        [Display(Name = "Vehicle Type")]
+        public string VehicleType { get; set; }
+        [Display(Name = "Registration Number")]
+        public string RegistrationNumber { get; set; }
+
+        [Display(Name = "Arrival Time")]
+        [DataType(DataType.DateTime)]
+        public DateTime ArrivalTime { get; set; }
+
+        [Display(Name = "Time Parked")]
+        [StringSyntax("TimeSpanFormat")]
+        public TimeSpan ParkedTime { get; set; }
+    }
+}
