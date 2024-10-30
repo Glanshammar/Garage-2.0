333b5c0abcb385d3c00d909a36c3ff62d01bf918
Andreas Johansson <glanshammar70@gmail.com>
2024-10-30 14:59:53 +0100

    Added new migration

---
+++ Garage 2.0/Migrations/20241030130609_Garage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Garage_2._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class Garage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParkedVehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleType = table.Column<int>(type: "int", nullable: false),
+                    RegistrationNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberOfWheels = table.Column<int>(type: "int", nullable: false),
+                    ArrivalTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkedVehicle", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ParkedVehicle",
+                columns: new[] { "Id", "ArrivalTime", "Brand", "Color", "Model", "NumberOfWheels", "RegistrationNumber", "VehicleType" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 30, 14, 6, 8, 649, DateTimeKind.Local).AddTicks(817), "Volkswagen", "Blue", "Jetta", 4, "ABC123", 0 },
+                    { 2, new DateTime(2024, 10, 30, 14, 6, 8, 649, DateTimeKind.Local).AddTicks(877), "Volkswagen", "Blue", "Taos", 4, "DEF123", 0 },
+                    { 3, new DateTime(2024, 10, 30, 14, 6, 8, 649, DateTimeKind.Local).AddTicks(882), "Something", "Black", "OrOther", 2, "CTF345", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParkedVehicle");
+        }
+    }
+}
