924a203bbf3a4078f1a50f0447373f3da1518c56
Andreas Johansson <glanshammar70@gmail.com>
2024-10-31 12:26:04 +0100

    Added ViewModel for search & sorting

---
+++ Garage 2.0/Models/ViewModels/VehicleSearchViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Garage_2._0.Models.ViewModels;
+
+public class VehicleSearchViewModel
+{
+    public string SearchString { get; set; }
+    public VehicleType? SortBy { get; set; }
+    public List<ParkedVehicleIndexViewModel> Vehicles { get; set; }
+}
