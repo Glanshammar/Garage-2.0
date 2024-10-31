5c30ab52eb6977572267468afb8c1fe9ff5f5616
Andreas Johansson <glanshammar70@gmail.com>
2024-10-31 10:11:55 +0100

    Added Checkout ViewModel

---
+++ Garage 2.0/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage_2._0.Models.ViewModels;
+
+public class CheckoutViewModel
+{
+    public int Id { get; set; }
+    public VehicleType VehicleType { get; set; }
+    public string RegistrationNumber { get; set; }
+    public DateTime ArrivalTime { get; set; }
+    public TimeSpan ParkedTime { get; set; }
+    public DateTime CheckoutTime { get; set; }
+}
