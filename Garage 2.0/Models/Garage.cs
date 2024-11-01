e40bd8d411d2547a443c6d543a7abf9b82b530b8
Karin Wegner <karinwegner00@gmail.com>
2024-11-01 12:28:43 +0100

    Created Garage and ParkingSpot Models

---
+++ Garage 2.0/Models/Garage.cs
@@ -0,0 +1,38 @@
+﻿namespace Garage_2._0.Models
+{
+    public class Garage
+    {
+        public int Id { get; set; }
+        public ParkingSpot[,] ParkingSpots;
+        public int numberOfParkingSpots {  get; set; }
+        public int vehiclesPerRow { get; set; } //number of columns
+        public int numberOfRows { get; set; } //number of rows
+
+        public Garage(int spots, int columnCount) 
+        {
+            numberOfParkingSpots = spots;
+            vehiclesPerRow = columnCount;
+            numberOfRows = Convert.ToInt32(spots/columnCount);
+        }
+
+        public void BuildGarage()
+        {
+            int finalRowSpots;
+
+            numberOfRows = Convert.ToInt32(numberOfParkingSpots / vehiclesPerRow);
+            finalRowSpots = numberOfParkingSpots % vehiclesPerRow;
+
+            ParkingSpots = new ParkingSpot[vehiclesPerRow, numberOfRows];
+
+            for (int y = 0; y < numberOfRows; y++)
+            {
+                for (int x = 0; x < vehiclesPerRow; x++)
+                {
+                    ParkingSpots[x, y] = new ParkingSpot(y, x, x+y);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Garage 2.0/Models/ParkingSpot.cs
@@ -0,0 +1,24 @@
+﻿namespace Garage_2._0.Models
+{
+    public class ParkingSpot
+    {
+        public int Id { get; set; }
+
+        //Rad och kolumn för grid placement av parkeringsplatser
+        public int row { get; set; }
+        public int column { get; set; }
+        public bool occupied { get; set; }
+
+        public ParkedVehicle? parkedVehicle { get; set; } //Är null ifall platsen är ledig
+
+        public ParkingSpot(int row, int column, int id)
+        {
+            Id = id;
+            this.row = row;
+            this.column = column;
+            occupied = false;
+            parkedVehicle = null;
+
+        }
+    }
+}
