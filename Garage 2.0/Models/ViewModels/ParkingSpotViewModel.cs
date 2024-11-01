b879ac8ecab23bd18dae157e4464c31b7beeeb64
Karin Wegner <karinwegner00@gmail.com>
2024-11-01 15:41:12 +0100

    Created ParkingSpotViewModel

---
+++ Garage 2.0/Models/ViewModels/ParkingSpotViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Garage_2._0.Models.ViewModels
+{
+    public class ParkingSpotViewModel
+    {
+        public IEnumerable<ParkingSpot> parkingSpotModel { get; set; }
+        public Garage garage { get; set; }
+
+
+        //Bestämmer hur breda 
+        public ParkingSpotViewModel(Garage garage, IEnumerable<ParkingSpot> parkingSpotModel)
+        {
+            this.garage = garage;
+                this.parkingSpotModel = parkingSpotModel;
+        }
+
+    }
+}
