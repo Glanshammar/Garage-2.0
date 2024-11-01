b5b00f18f773acb5af41221f58807140e503649f
Karin Wegner <karinwegner00@gmail.com>
2024-11-01 15:39:55 +0100

    Created ParkinSpotsController + context and Migration

---
+++ Garage 2.0/Controllers/ParkingSpotsController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Garage_2._0.Data;
+using Garage_2._0.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Garage_2._0.Controllers
+{
+    public class ParkingSpotsController : Controller
+    {
+        private readonly Garage_2_0ParkingSpotContext _context;
+        public Garage? garage = null;
+        public ParkingSpotsController(Garage_2_0ParkingSpotContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ParkingSpots
+        public async Task<IActionResult> Index()
+        {
+            if (garage is null)
+            {
+               await CreateGarage();
+            }
+
+            var model = new ParkingSpotViewModel(garage, await _context.ParkingSpot.ToListAsync());
+
+            return View(model);
+        }
+
+        // GET: ParkingSpots/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkingSpot = await _context.ParkingSpot
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parkingSpot == null)
+            {
+                return NotFound();
+            }
+
+            return View(parkingSpot);
+        }
+
+        // GET: ParkingSpots/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParkingSpots/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,row,column,occupied")] ParkingSpot parkingSpot)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(parkingSpot);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parkingSpot);
+        }
+
+        // GET: ParkingSpots/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkingSpot = await _context.ParkingSpot.FindAsync(id);
+            if (parkingSpot == null)
+            {
+                return NotFound();
+            }
+            return View(parkingSpot);
+        }
+
+        // POST: ParkingSpots/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,row,column,occupied")] ParkingSpot parkingSpot)
+        {
+            if (id != parkingSpot.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parkingSpot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParkingSpotExists(parkingSpot.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parkingSpot);
+        }
+
+        // GET: ParkingSpots/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkingSpot = await _context.ParkingSpot
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parkingSpot == null)
+            {
+                return NotFound();
+            }
+
+            return View(parkingSpot);
+        }
+
+        // POST: ParkingSpots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var model = new ParkingSpotViewModel(garage, await _context.ParkingSpot.ToListAsync());
+            
+
+            var parkingSpot = await _context.ParkingSpot.FindAsync(id);
+            if (parkingSpot != null)
+            {
+                _context.ParkingSpot.Remove(parkingSpot);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ParkingSpotExists(int id)
+        {
+            return _context.ParkingSpot.Any(e => e.Id == id);
+        }
+
+        public async Task<IActionResult> CreateGarage(int spots = 15, int columns = 5)
+        {
+            
+             garage = new Garage(spots, columns);
+          
+            for (int y = 0; y < garage.numberOfRows; y++)
+            {
+                for (int x = 0; x < garage.vehiclesPerRow; x++)
+                {
+                    garage.ParkingSpots[y,x] = new ParkingSpot(y, x);
+                    _context.Add(garage.ParkingSpots[y, x]);
+                }
+
+            }
+            var model = _context.ParkingSpot.ToListAsync();
+
+            return View(nameof(Index),await model);
+
+        }
+    }
+}
---
+++ Garage 2.0/Data/Garage_2_0ParkingSpotContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage_2._0.Models;
+using Garage_2._0.Migrations;
+using Garage_2._0.Controllers;
+
+namespace Garage_2._0.Data
+{
+    public class Garage_2_0ParkingSpotContext : DbContext
+    {
+        Models.Garage garage = new Models.Garage(15,5);
+        public Garage_2_0ParkingSpotContext (DbContextOptions<Garage_2_0ParkingSpotContext> options )
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage_2._0.Models.ParkingSpot> ParkingSpot { get; set; } = default!;
+    }
+}
