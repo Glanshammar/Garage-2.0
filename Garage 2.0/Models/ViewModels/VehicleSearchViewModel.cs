326e1dfe732d6ca283e687f587de1707636ded5a
Andreas Johansson <glanshammar70@gmail.com>
2024-11-01 06:13:03 +0100

    Removed unused viewmodel

--- Garage 2.0/Models/ViewModels/VehicleSearchViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Garage_2._0.Models.ViewModels;
-
-public class VehicleSearchViewModel
-{
-    public string SearchString { get; set; }
-    public VehicleType? SortBy { get; set; }
-    public List<ParkedVehicleIndexViewModel> Vehicles { get; set; }
-}
