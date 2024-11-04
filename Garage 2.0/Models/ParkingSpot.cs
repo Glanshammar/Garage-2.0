fb2f959ea338a37717c4bba2d48b4b0cdce6f845
Karin Wegner <karinwegner00@gmail.com>
2024-11-04 12:05:00 +0100

    Added ParkingSpotID Removed ParkedVehicle variable from ParkingSpot

--- Garage 2.0/Models/ParkingSpot.cs
+++ Garage 2.0/Models/ParkingSpot.cs
@@ -2,21 +2,23 @@
 {
     public class ParkingSpot
     {
-        public int Id { get; set; }
+        public int ParkingSpotId { get; set; }
 
         //Rad och kolumn för grid placement av parkeringsplatser
         public int row { get; set; }
         public int column { get; set; }
-        public bool occupied { get; set; }
+        public bool occupied { get; set; } = false;
 
-        public ParkedVehicle? parkedVehicle { get; set; } //Är null ifall platsen är ledig
+        
 
-        public ParkingSpot(int row, int column)
+        //public ParkedVehicle? parkedVehicle { get; set; } //Är null ifall platsen är ledig
+
+        public ParkingSpot(int row, int column, int id, bool occupied)
         {
             this.row = row;
             this.column = column;
-            occupied = false;
-            parkedVehicle = null;
+            ParkingSpotId = id;
+            this.occupied = occupied;
 
         }
     }
