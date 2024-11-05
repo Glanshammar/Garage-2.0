a120e79598d9b1df1abe73f4b5f0d036b4036136
Andreas Johansson <glanshammar70@gmail.com>
2024-11-05 11:12:29 +0100

    Refactoring of code

--- Garage 2.0/Models/ViewModels/ReceiptViewModel.cs
+++ Garage 2.0/Models/ViewModels/ReceiptViewModel.cs
@@ -1,14 +1,29 @@
-﻿namespace Garage_2._0.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models.ViewModels
 {
     public class ReceiptViewModel
     {
         public int Id { get; set; }
+        
+        [Display(Name = "Vehicle Type")]
         public VehicleType VehicleType { get; set; }
+        
+        [Display(Name = "Registration Number")]
         public string RegistrationNumber { get; set; }
+        
+        [Display(Name = "Arrival Time")]
         public DateTime ArrivalTime { get; set; }
+        
+        [Display(Name = "Parked Time")]
         public TimeSpan ParkedTime { get; set; }
+        
+        [Display(Name = "Checkout Time")]
         public DateTime CheckoutTime { get; set; }
-        public decimal Price { get; set; } // The calculated parking price
+        
+        [Display(Name = "Price ")]
+        public decimal Price { get; set; }
+        
         public bool IsConfirmation { get; set; }
     }
 }
